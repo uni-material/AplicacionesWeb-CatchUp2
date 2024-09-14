f56169673aa80f9b391a72e853e67495209903b2
Ro <u202213765@upc.edu.pe>
2024-09-13 21:36:51 -0500

    feat: implemented unavailable-content.component.vue components

---
+++ src/news/components/unavailable-content.component.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+  name: "unavailable-content",
+  props: {
+    errors: null,
+  }
+}
+</script>
+<template>
+  <div>
+    <div><h4>News service is unavailable now.</h4></div>
+    <div v-for="error in errors">
+      <h6>{{ error }}</h6>
+    </div>
+  </div>
+</template>
+<style scoped>
+</style>
